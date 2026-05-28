170e2aaf55a2d43de32ff72153c4aa94e9991078
mhl004 <mh.lisowska@gmail.com>
2026-05-28 20:11:26 +0200

    Update Strona.py

--- Strona.py
+++ Strona.py
@@ -13,21 +13,25 @@ st.markdown("""
     border-color: #4169E1;
     background-color: #00FFFFF;
 }
+* {
+    font-family: "Yu Gothic" !important;
+}
+
 </style>
 """, unsafe_allow_html=True)
 
 
-st.title("Strona główna")
+st.title("Trafione oferty, czyli promocje na podstawie historii zakupów")
 st.write('\n')
 st.write('\n')
-st.write('<u>REGUŁY ASOCJACYJNE</u>', unsafe_allow_html=True)
+st.write('**<u>REGUŁY ASOCJACYJNE</u>**', unsafe_allow_html=True)
 st.write('W tej zakładce użytkownik dobiera kategorię oraz miesiąc. W wyniku działania modelu, strona zwraca pary lub zestawy konkretnych produktów, dla których opłaca się wprowadzać promocje łączone. W przypadku nieznalezienia istotnych zależności, użytkownik uzyskuje informacje na temat innych sugestii działania.')
-st.page_link(page='pages/Reguly.py',label = "Reguły")
+st.page_link(page='pages/Reguly.py',label = "**Reguły**")
 st.write('\n')
 st.write('\n')
-st.write('<u>ANALIZA KATEGORII</u>', unsafe_allow_html=True)
+st.write('**<u>ANALIZA KATEGORII</u>**', unsafe_allow_html=True)
 st.write('W tej zakładce użytkownik wybiera kategorię. Dla niej program znajduje miesiące, w których istnieją istotne powiązania i predyspozycje do wprowadzenia promocji łączonych.')
-st.page_link(page='pages/Kategorie.py',label = "Kategorie")
+st.page_link(page='pages/Kategorie.py',label = "**Kategorie**")
 st.write('\n')
 st.write('\n')
 
